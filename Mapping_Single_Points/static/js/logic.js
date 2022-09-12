596b86b205cda7c4fb7467a2c3264e234ecb1093
Nensi Pandya <nensirph@gmail.com>
2022-09-11 20:16:34 -0400

    adding new data

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -24,8 +24,3 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tile
     maxZoom: 18,
     accessToken: API_KEY
 });
-
-// Then we add our 'graymap' tile layer to the map.
-
-streets.addTo(map);
-F
