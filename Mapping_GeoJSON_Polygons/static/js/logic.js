01847633c75aced7cdcc4fc7cf5f0e4ad77bdb4d
Nensi Pandya <nensirph@gmail.com>
2022-09-12 09:26:50 -0400

    add the data

--- Mapping_GeoJSON_Polygons/static/js/logic.js
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -1,5 +1,5 @@
 // // Add console.log to check to see if our code is working.
-// console.log("working");
+console.log("working");
 
 // // Create the map object with a center and zoom level.
 // let map = L.map('mapid').setView([30, 30], 2);
@@ -40,7 +40,7 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
 
 
 // We create the dark view tile layer that will be an option for our map.
-let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let satellitestreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -48,15 +48,15 @@ let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-  Street: streets,
-  Light: light
+  "Streets": streets,
+  "Satellite Streets": satellitestreets
 };
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-  center: [44.0, -80.0],
-  zoom: 2,
-  layers: [light]
+  center: [43.7, -79.3],
+  zoom: 11,
+  layers: [satellitestreets]
 })
 
 // Pass our map layers into our layers control and add the layers control to the map.
@@ -66,23 +66,29 @@ L.control.layers(baseMaps).addTo(map);
 // streets.addTo(map);
 
 // Accessing the airport GeoJSON URL
-let rtData = "https://raw.githubusercontent.com/nensipandya/Mapping_Earthquakes/main/torontoRoutes.json";
-// let torontoData = "https://github.com/nensipandya/Mapping_Earthquakes/blob/main/torontoRoutes.json";
+let torontoHoods = "https://raw.githubusercontent.com/nensipandya/Mapping_Earthquakes/master/torontoNeighborhoods.json";
+// let torontoHoods = "https://github.com/nensipandya/Mapping_Earthquakes/blob/main/torontoNeighborhoods.json";
 
 // Create a style for the lines.
 let myStyle = {
     color: "#ffffa1",
-    weight: 2
+    weight: 1
   }
   
 // Grabbing our GeoJSON data.
-d3.json(torontoData).then(function(data) {
-  L.geoJson(data,{
-    style : myStyle,
-    onEachFeature : function(feature,layer) {
-  layer.bindPopup("<h3> Airline : "+ feature.properties.airline + "</h3> <hr> <h3> Destination:" +feature.properties.dst +"</h3>");
-  }
-  }).addTo(map);
-  });
+d3.json(torontoHoods).then(function(data) {
+    console.log(data);
+    //Creating a GepJSON layer with the retrieved data.
+    L.geoJSON(data).addTo(map);
+});
+
+// Grabbing our GeoJSON data and a popup
+d3.json(torontoHoods).then(function(data) {
+    L.geoJson(data,{
+      style : myStyle,
+      onEachFeature : function(feature,layer) {
+    layer.bindPopup("<h3> Neighborhood : "+ feature.properties.AREA_NAME + "</h3>");
+    }
+    }).addTo(map);
+    });
 Footer
-;
